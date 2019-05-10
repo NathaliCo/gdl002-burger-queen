e6955a4a417e8f5b90022050c8d2dd93a4df7931
unknown <nathaliecortz@hotmail.com>
2019-05-10 13:19:01 -0500

    Updates

---
+++ precache-manifest.498c0b65d89ce8a2320b4165de215364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b05073fef54838da709d262e6f0bf2",
+    "url": "/gdl002-burger-queen/index.html"
+  },
+  {
+    "revision": "0557ee8bb237c7a55f45",
+    "url": "/gdl002-burger-queen/static/css/main.9c74a74c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/gdl002-burger-queen/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "0557ee8bb237c7a55f45",
+    "url": "/gdl002-burger-queen/static/js/main.2163ed6a.chunk.js"
+  },
+  {
+    "revision": "abf1e5c4314e6441e9d2",
+    "url": "/gdl002-burger-queen/static/js/runtime~main.e107886b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gdl002-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
