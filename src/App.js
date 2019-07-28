ca892a65d6b283d50dfedf111f1f3112cb2c924d
Nathalie <nathaliecortz@hotmail.com>
2019-07-27 19:47:44 -0500

    Add backgound to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 import WaiterPage from "./Components/waiterPage";
 import Header from "./Components/Header";
 import ChefPage from "./Components/ChefPage";
+import backgroundHead from "./Components/assets/backgroundHead.jpg"
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <img className='bg' src={backgroundHead} />
       <Route exact path="/Waiter" component={WaiterPage} />
       <Route path="/chef" component={ChefPage} />
     </BrowserRouter>
